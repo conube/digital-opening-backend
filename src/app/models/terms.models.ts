247d84ee17753995393ab34e3497d66b002a5385
Fabio Santos <fabioprogramadorti@gmail.com>
2020-12-09 20:33:26 -0300

    Create model schema of terms

---
+++ src/app/models/terms.models.ts
@@ -0,0 +1,27 @@
+import { Document, Schema, model, Types } from 'mongoose'
+
+export interface ITerms {
+	type: [],
+	version: String,
+	content: String
+}
+
+export interface ITermsSchema extends ITerms, Document { }
+
+const TermsSchema: Schema = new Schema({
+	type: {
+		type: String,
+		enum: ['USE', 'PRIVACY'],
+		required: true,
+	},
+	version: {
+		type: 'String',
+		required: true
+	},
+	content: {
+		type: 'String',
+		required: true
+	}
+}, { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } })
+
+export const TermsModel = model<ITermsSchema>('Terms', TermsSchema)
